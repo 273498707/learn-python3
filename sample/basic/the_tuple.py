6646230dbbebc406adc6cb5ab708fc38944ae63b
John <John@qq.com>
2017-01-17 00:46:36 +0800

    add the_tuple.py

---
+++ sample/basic/the_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+classmates = ('Michael', 'Bob', 'Tracy')
+#print('classmates= %s' %(classmates) )
+print('classmates= ', classmates)
+print('len(classmates) = %d' %(len(classmates)) )
+print('classmates[0]= %s' %(classmates[0]) )
+print('classmates[1]= %s' %(classmates[1]) )
+print('classmates[2]= %s' %(classmates[2]) )
+print('classmates[-1]= %s' %(classmates[-1]) )
+
+#cannot modify tuple:
+#classmates[0] = 'Adam'
