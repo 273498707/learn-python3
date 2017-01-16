da723b28b6f9ef528912708c25a5cc9cfbc77ee0
John <John@qq.com>
2017-01-16 23:27:33 +0800

    add the_list.py

---
+++ sample/basic/the_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+classmates= ['Michael', 'Bob', 'Tracy']
+print('classmates= %s'%classmates)
+print('len(classmates)= %d' %len(classmates))
+print('classmates[0]= %s' %(classmates[0]))
+print('classmates[1]= %s' %(classmates[1]) )
+print('classmates[2]= %s' %(classmates[2]) )
+print('classmates[-1]= %s' %(classmates[-1]) )
+classmates.pop()
+print('classmates= %s' %(classmates))
