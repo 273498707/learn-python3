85421d366da4fabc722d451c6e06ba08e401dbcd
John <John@qq.com>
2017-01-17 21:19:23 +0800

    modified: the_tuple.py,apped:list

--- sample/basic/the_tuple.py
+++ sample/basic/the_tuple.py
@@ -10,5 +10,10 @@ print('classmates[1]= %s' %(classmates[1]) )
 print('classmates[2]= %s' %(classmates[2]) )
 print('classmates[-1]= %s' %(classmates[-1]) )
 
+tuples = ('a', 'b', ['A', 'B'])
+print('tuples= ', tuples)
+tuples[2][0] = 'X'
+tuples[2][1] = 'Y'
+print('tuples= ', tuples)
 #cannot modify tuple:
 #classmates[0] = 'Adam'
