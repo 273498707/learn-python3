bca93cc2cd30f2cb33d7cb7bfa687883b597ad7a
273498707 <273498707@qq.com>
2017-01-28 02:17:20 +0800

    添加 platform 模块，以获得python版本信息.

--- sample/basic/hello.py
+++ sample/basic/hello.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- conding: utf-8 -*-
 
+import platform
+
+print(platform.python_version())
+
 print('Hello, world')
 print('Hello, ubuntu')
 print('Hello, tortoisegit')
