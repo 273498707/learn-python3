5098231c134ab5316dce69df057c91ef0bc82881
273498707 <273498707@qq.com>
2017-01-28 02:18:14 +0800

    添加 platform 模块 显示 python版本

--- sample/basic/do_if.py
+++ sample/basic/do_if.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+
+import platform
+
+print(platform.python_version())
 
 age = 20 
 if age >= 18:
     print('Your age is %s' %age)
     print('adult')
+
+
 
