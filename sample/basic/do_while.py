be765396c7096470ba2bf7dbbd108e917605319c
273498707 <273498707@qq.com>
2017-01-29 01:22:10 +0800

    计算1x2x3x...x100

--- sample/basic/do_while.py
+++ sample/basic/do_while.py
@@ -5,7 +5,15 @@
 sum = 0
 n = 1
 while n <= 100:
-	sum = sum + n
-	n = n + 1
+    sum = sum + n
+    n = n + 1
 print('1+2+3+...+100 = %d' %sum)
+
+# 计算1x2x3x...x100
+acc = 1
+n = 1
+while n <= 100:
+    acc = acc * n
+    n += 1
+print('1x2x3x...x100 = %d' %acc)
 
