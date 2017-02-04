8095eb154f51c721f061c7707a2bab637cb9ac09
John <John@qq.com>
2017-02-05 00:44:08 +0800

    add def_func.py

---
+++ sample/function/def_func.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+
+def my_abs(x):
+	result = x
+	if x > 0: result = x
+	elif x = 0: result = 0
+	else: result = -x
+	return result
+n = -10
+print(my_abs(n1))
