171c47be27c04733061e029e71e122f778c02754
John <John@qq.com>
2017-02-05 00:37:55 +0800

    add test hex()

--- sample/function/call_func.py
+++ sample/function/call_func.py
@@ -8,3 +8,9 @@ print(x,y)
 print('max(1,2) = %d' %max(1, 2))
 print('min(2, 3, 1, -5) = %d' %min(2, 3, 1, -5))
 print('sum([1, 2, 3, 4]) = %d' %sum([1, 2, 3, 4]))
+
+#how to hex()
+n1 = 255
+n2 = 1000
+print('doe2hex 255d to hex = %s' %hex(n1))
+print('doe2hex 1000d to hex = %s' %hex(n2))
