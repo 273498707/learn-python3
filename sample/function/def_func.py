c3f92ca3f887cf6c74f0915ded47151bfa040ea2
John <John@qq.com>
2017-02-05 00:48:27 +0800

    append move()

--- sample/function/def_func.py
+++ sample/function/def_func.py
@@ -10,5 +10,12 @@ def my_abs(x):
         return x
     else:
         return -x
-n = -10
+n = -10.0
 print(my_abs(n))
+
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+x, y = move(100, 100, 60, math.pi / 6)
+print(x, y)
