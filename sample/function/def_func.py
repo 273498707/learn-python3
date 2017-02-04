2bd6e5127a308fb966b055ee7512185a70329e65
John <John@qq.com>
2017-02-05 00:45:05 +0800

    moddfy my_abs(x)

--- sample/function/def_func.py
+++ sample/function/def_func.py
@@ -6,8 +6,8 @@ import math
 def my_abs(x):
 	result = x
 	if x > 0: result = x
-	elif x = 0: result = 0
+	elif x == 0: result = 0
 	else: result = -x
 	return result
 n = -10
-print(my_abs(n1))
+print(my_abs(n))
